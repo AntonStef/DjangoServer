686aeb6267027f6d555dd4a78e36758355b33f8b
AntonStef <antonstefanidi@mail.ru>
2020-03-26 23:46:38 +0300

    add comment for foreign key

--- myDjango/catalog/models.py
+++ myDjango/catalog/models.py
@@ -24,6 +24,10 @@ class Book(models.Model):
     Model representing a book (but not a specific copy of a book).
     """
     title = models.CharField(max_length=200)
+    # FOREIGN KEY организует ссылочную целостность для
+    # связи один - ко - многим реже один - к - один).
+    # При этом на стороне "один" создаётся уникальный(обычно - это первичный) индекс,
+    # а на стороне "много" - внешний ключ(FK) на этот индекс.
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file.
