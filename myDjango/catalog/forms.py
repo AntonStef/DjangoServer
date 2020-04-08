6acd69695fbb8b0a8d6475fd2c19f1131c73796e
AntonStef <antonstefanidi@mail.ru>
2020-04-08 19:49:00 +0300

    added forms and views for updating Book, Author and borrow date

---
+++ myDjango/catalog/forms.py
@@ -0,0 +1,65 @@
+from django import forms
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+import datetime  # for checking renewal date range.
+
+from django.forms import ModelForm
+from .models import BookInstance
+
+# создание формы на основе класса Form
+class RenewBookForm(forms.Form):
+    renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).", label='New date')
+
+    # создаем дополнительную поля поля renewal_date из формы, что бы оно было не позже текущего дня
+    # и не позже 4-х недель вперед
+    # мето должен иметь имя clean_<fieldname>(), где fieldname - это интересующее нас поле
+    # в данном случае renewal_date
+    def clean_renewal_date(self):
+        # Данный шаг позволяет нам, при помощи валидаторов, получить "очищенные", проверенные,
+        # а затем, приведенные к стандартным типам, данные
+        # в данном случае в datetime.datetime
+        # print(type(self.cleaned_data['renewal_date']), self.cleaned_data['renewal_date'])
+        data = self.cleaned_data['renewal_date']
+
+        # Проверка того, что дата не выходит за "нижнюю" границу (не в прошлом).
+        if data < datetime.date.today():
+            raise ValidationError(_('Invalid date - renewal in past'))
+
+        # Проверка того, то дата не выходит за "верхнюю" границу (+4 недели).
+        if data > datetime.date.today() + datetime.timedelta(weeks=4):
+            raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
+
+        # Помните, что всегда надо возвращать "очищенные" данные.
+        # print('data', data)
+        return data
+
+# создание формы на основе уже существующей модели
+# это намного легче на мой взгляд
+# этот класс явялется эквивалентом RenewBookForm, только он на основе модели
+class RenewBookModelForm(ModelForm):
+
+    # создается метод для валидации конкретного поля формы. Поскольку тут нет объявления полей, а они берутся из модели
+    # поэтому мы пишем уже не clean_renewal_date, а clean_due_back потому что поле модели "due_back"
+    def clean_due_back(self):
+        data = self.cleaned_data['due_back']
+
+        # Проверка того, что дата не в прошлом
+        if data < datetime.date.today():
+            raise ValidationError(_('Invalid date - renewal in past (!using Model!)'))
+
+        # Check date is in range librarian allowed to change (+4 weeks)
+        if data > datetime.date.today() + datetime.timedelta(weeks=4):
+            raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead (!using Model!)'))
+
+        # Не забывайте всегда возвращать очищенные данные
+        return data
+
+    class Meta:
+        model = BookInstance
+        # происываем поля которые хотим отсавим в форме
+        fields = ['due_back', ]
+        # значением атребутов для полей формы указываем следующим образом
+        # атребут = {поле: значене}
+        labels = {'due_back': _('Renewal date custom'), }
+        help_texts = {'due_back': _('Enter a date between now and 4 weeks (default 3).'), }
--- myDjango/catalog/views.py
+++ myDjango/catalog/views.py
@@ -4,12 +4,28 @@ from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
+
+from django.contrib.auth.decorators import permission_required
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import datetime
+
+from .forms import RenewBookForm, RenewBookModelForm
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
+from django.utils.translation import ugettext_lazy as _
+
+
 # render - функция которая генерирует HTML-файлы при помощи
 # шаблонов страниц и соотвествующих данных
 
 
 # Create your views here.
-# декоратор для аутентификации пользователя
+# декоратор для аутентификации пользователяs
 # работает для методов
 @login_required
 def index(request):
@@ -155,3 +171,114 @@ class AllLoanedBooksForLibrarianView(PermissionRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+
+# для ограничения доступа используется декоратор, поскольку у нас метод
+# для классов происходит наследование у класса PermissionRequiredMixin
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    """
+    View function for renewing a specific BookInstance by librarian
+    """
+    book_inst = get_object_or_404(BookInstance, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        # print(request.POST['renewal_date'])
+
+        # 1 способ
+        # print(request.POST)
+        form = RenewBookForm(request.POST)
+        # print(form)
+        # 2 способ
+        # form = RenewBookModelForm(request.POST)
+
+        # Check if the form is valid:
+        # здесь происходит и внутрення валидация (Django) и дополнитльная валидация, которую создал я внутри формы
+        # RenewBookForm --> метод clean_renewal_date
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+
+            # 1 способ
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            # 2 способ
+            # book_inst.due_back = form.cleaned_data['due_back']
+
+            book_inst.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        # 1 способ
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date, })
+        # 2 способ
+        # form = RenewBookModelForm(initial={'due_back': proposed_renewal_date, })
+
+    return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst': book_inst})
+
+# Обобщенные классы отображения для редактирования
+# синтаксис такой как и у форм, которые наслеюутся у ModelForm
+# Отображения  "создать" и "обновить" используют  шаблоны с именем model_name_form.html,
+# по умолчанию: (вы можете поменять суффикс на что-нибудь другое, при помощи поля
+# template_name_suffix в вашем отображении, например, template_name_suffix = '_other_suffix')
+#  я ничего не менял, поэтому название шаблона должно быть author_form.html, поскольку модель Author
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+
+    # указываем разрешения
+    permission_required = ('catalog.can_update_create_delete_author', )
+
+    fields = '__all__'
+    initial = {'date_of_death': '12/10/1500', }
+
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author
+
+    # указываем разрешения
+    permission_required = ('catalog.can_update_create_delete_author', )
+
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+# Отображения "удалить" ожидает "найти" шаблон с именем формата model_name_confirm_delete.html
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    # указываем разрешения
+    permission_required = ('catalog.can_update_create_delete_author', )
+
+    success_url = reverse_lazy('authors')
+
+
+class BookCreate(PermissionRequiredMixin, CreateView):
+    model = Book
+
+    # указываем разрешения
+    permission_required = ('catalog.can_update_create_delete_book', )
+
+    fields = '__all__'
+    # initial = {'date_of_death': '12/10/1500', }
+    initial = {'summary': 'Очень интересная книга раз ты ее добавляешь!', }
+
+
+class BookUpdate(PermissionRequiredMixin, UpdateView):
+    model = Book
+
+    # указываем разрешения
+    permission_required = ('catalog.can_update_create_delete_book', )
+    fields = '__all__'
+
+    # fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+# Отображения "удалить" ожидает "найти" шаблон с именем формата model_name_confirm_delete.html
+class BookDelete(PermissionRequiredMixin, DeleteView):
+    model = Book
+    # указываем разрешения
+    permission_required = ('catalog.can_update_create_delete_book', )
+
+    success_url = reverse_lazy('books')
